c1ca781058473bd41b2aa3a93f26e4f39513f1a5
Idris Westbrook <westbrook.idris@gmail.com>
2022-09-01 12:40:53 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById('root'))
+ReactDOM.render(<ul><h2>A list!</h2><li>An item!</li><li>Another item!</li></ul>, document.getElementById('list'))
